44d6d0daa73924b989c7f646ec2b1829889f849e
Karzan Botani <karzan@botani.nu>
2023-02-19 10:11:16 +0100

    feat: add TextArea component

---
+++ web/src/lib/components/TextArea.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let value = '';
+	export let placeholder = '';
+	export let id = '';
+	export let label = '';
+	export let disabled = false;
+	export let required = false;
+	export let errors: string[] | undefined = [];
+</script>
+
+<div class="form-control w-full max-w-lg">
+	<label for={id} class="label font-medium pb-1">
+		<span class="label-text">{label}</span>
+	</label>
+
+	<textarea
+		{disabled}
+		{id}
+		{placeholder}
+		{required}
+		{value}
+		name={id}
+		class="textarea textarea-bordered h-24 resize-none"
+	/>
+
+	{#if errors}
+		{#each errors as error}
+			<label for={id} class="label py-0 pt-1">
+				<span class="label-text-alt text-error">{error}</span>
+			</label>
+		{/each}
+	{/if}
+</div>
