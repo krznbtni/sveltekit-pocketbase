6fa68a70cbdafd209e233fd353bdbe45c89148e8
Karzan Botani <karzan@botani.nu>
2023-02-19 15:18:04 +0100

    refactor: base page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import { ProjectCard } from '$lib/components';
+	import { HomeDiscover } from '$lib/components';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<div class="flex flex-col gap-3">
+<div class="w-full mt-4 space-y-6">
 	<h1 class="text-xl font-bold">Recent Projects</h1>
 
-	<div class="grid grid-cols-3 gap-2">
-		{#each data.projects as project}
-			<ProjectCard {project} />
-		{/each}
-	</div>
+	<HomeDiscover projects={data.projects} />
 </div>
