d0fe0fa82904f8a1ebf8755fe899b320aeab9fb7
Karzan Botani <karzan@botani.nu>
2023-02-19 15:17:53 +0100

    refactor: ProjectCard

--- web/src/lib/components/ProjectCard.svelte
+++ web/src/lib/components/ProjectCard.svelte
@@ -5,9 +5,10 @@
 	export let project: Project;
 </script>
 
-<div class="card w-72 bg-base-100 shadow-xl">
-	<figure>
+<div class="card card-compact bg-base-100 shadow-lg">
+	<figure class="h-48">
 		<img
+			class="object-cover object-center w-full h-full"
 			src={project.thumbnail
 				? getImageURL(project.collectionId, project.id, project.thumbnail, '80x80')
 				: `https://via.placeholder.com/500/4506CB/FFFFFF/?text=${project.name}`}
@@ -15,10 +16,14 @@
 		/>
 	</figure>
 
-	<div class="card-body">
-		<h2 class="card-title">{project.name}</h2>
+	<div class="card-body gap-0">
+		<a href="/projects/{project.id}" class="card-title">
+			{project.name}
+		</a>
+
 		<p>{project.tagline}</p>
-		<div class="card-actions justify-end">
+
+		<div class="card-actions justify-end space-x-2">
 			<a href="/projects/{project.id}" class="btn btn-primary">View Project</a>
 		</div>
 	</div>
