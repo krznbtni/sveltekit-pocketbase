92a62675f8aef86f0b9ce0d912b364814f0c02a6
Karzan Botani <karzan@botani.nu>
2023-02-19 10:11:53 +0100

    refactor: export TextArea from lib dir

--- web/src/lib/components/index.ts
+++ web/src/lib/components/index.ts
@@ -1,5 +1,6 @@
 import Input from '$lib/components/Input.svelte';
 import Modal from '$lib/components/Modal.svelte';
 import MyProjectItem from '$lib/components/MyProjectItem.svelte';
+import TextArea from '$lib/components/TextArea.svelte';
 
-export { Input, Modal, MyProjectItem };
+export { Input, Modal, MyProjectItem, TextArea };
