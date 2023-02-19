2ad9331d388fb7b3203290b89dd50bf7904bc45e
Karzan Botani <karzan@botani.nu>
2023-02-19 09:22:05 +0100

    feat: use new Zod validation util on login page

--- web/src/routes/login/+page.server.ts
+++ web/src/routes/login/+page.server.ts
@@ -1,4 +1,6 @@
-import { error, redirect } from '@sveltejs/kit';
+import { loginUserSchema } from '$lib/schemas';
+import { validateData } from '$lib/utils';
+import { error, fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 type RequestData = {
@@ -8,14 +10,18 @@ type RequestData = {
 
 export const actions: Actions = {
 	login: async ({ locals, request }) => {
-		const body: RequestData = Object.fromEntries(await request.formData());
+		// const body: RequestData = Object.fromEntries(await request.formData());
+		const { formData, errors } = await validateData(await request.formData(), loginUserSchema);
 
-		if (!body.email || !body.password) {
-			return;
+		if (errors) {
+			return fail(400, {
+				data: formData,
+				errors: errors.fieldErrors,
+			});
 		}
 
 		try {
-			await locals.pb.collection('users').authWithPassword(body.email, body.password);
+			await locals.pb.collection('users').authWithPassword(formData.email, formData.password);
 
 			if (!locals.pb?.authStore?.model?.verified) {
 				locals.pb.authStore.clear();
